651bc1372f8254b260adbbcbc8eace71ee0c52bd
Zaoomc <dinhtruonggiang24108@gmail.com>
2026-08-16 16:00:28 +0700

    Add LagFixConfig class with configuration constants

---
+++ src/main/java/com/example/lagfix/LagFixConfig.java
@@ -0,0 +1,12 @@
+package com.example.lagfix;
+
+public class LagFixConfig {
+
+    public static final double NEAR_RANGE = 32.0;
+    public static final double FAR_RANGE = 64.0;
+
+    public static final int MEDIUM_THROTTLE_INTERVAL = 4;
+    public static final int FAR_THROTTLE_INTERVAL = 10;
+
+    public static final boolean SKIP_IF_HAS_TARGET = true;
+}
